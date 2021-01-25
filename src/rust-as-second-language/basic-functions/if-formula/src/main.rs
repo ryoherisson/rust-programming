ab7f64aca516b2c94facae0397a67eddf2ea5a3b
ryoherisson <ryoherisson@users.noreply.github.com>
2021-01-26 01:41:48 +0900

    if

---
+++ src/rust-as-second-language/basic-functions/if-formula/src/main.rs
@@ -0,0 +1,14 @@
+fn factorial(n: usize) -> usize {
+    // ifは式なので関数の最後に置くと値を返せる
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n-1)
+    }
+}
+
+fn main() {
+    // ';'をつけることで文になり、コンパイルが通る
+    factorial(10);
+    println!("{}", factorial(10));
+}
