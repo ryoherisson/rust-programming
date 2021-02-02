68818d0a8f6ccb9c2523cc976da81d791a9ba455
ryoherisson <ryoherisson@users.noreply.github.com>
2021-02-02 22:19:31 +0900

    variable

--- src/rust-programming-language/03_general_programming/variable_mut/src/main.rs
+++ src/rust-programming-language/03_general_programming/variable_mut/src/main.rs
@@ -4,4 +4,18 @@ fn main() {
     println!("The value of x is {}", x);
     x = 6;
     println!("The value of x is {}", x);
+
+    const MAX_POINTS: u32 = 100_000;
+    println!("MAX_POINTS: {}", MAX_POINTS);
+
+    let y = 5;
+    println!("y: {}", y);
+    let y = y + 5;
+    println!("y+5: {}", y);
+    let y = y + 5;
+    println!("y+10: {}", y);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("spaces: {} end", spaces);
 }
