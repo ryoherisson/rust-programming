742765cfba7b1f2603d28e56acafdb2e998a7678
ryoherisson <ryoherisson@users.noreply.github.com>
2021-02-06 23:41:08 +0900

    match_flow

---
+++ src/rust-programming-language/06_enum/match_flow/src/main.rs
@@ -0,0 +1,54 @@
+#[derive(Debug)] // すぐに州を点検できるように
+enum UsState {
+    Alabama,
+    Alaska,
+    // ... などなど
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let coin = Coin::Penny;
+    value_in_cents(coin);
+    println!("Hello, world!");
+    let coin = Coin::Quarter(UsState::Alabama);
+    value_in_cents(coin);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    // _というパターンは、どんな値にもマッチする
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
