094a756a92fda7259fca9a9a0db367004d0306d1
ryoherisson <ryoherisson@users.noreply.github.com>
2021-02-07 15:06:02 +0900

    restaurant

--- src/rust-programming-language/07_pack_crate_module/restaurant/src/lib.rs
+++ src/rust-programming-language/07_pack_crate_module/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -14,6 +14,58 @@ mod front_of_house {
     }
 }
 
+// moduleを公開してもその中身は公開されない.
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     // 絶対パス
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // Relative path
+//     // 相対パス
+//     front_of_house::hosting::add_to_waitlist();
+// }
+
+fn serve_order() {}
+
+// Enum. Appetizerというenumを公開したので、SoupとSaladというヴァリアントもeat_at_restaurantで使える.
+// mod back_of_house {
+//     pub enum Appetizer {
+//         Soup,
+//         Salad,
+//     }
+// }
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    // 夏 (summer) にライ麦 (Rye) パン付き朝食を注文
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    // やっぱり別のパンにする
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // 下の行のコメントを外すとコンパイルできない。食事についてくる
+    // 季節のフルーツを知ることも修正することも許されていないので (pubじゃない)
+    // meal.seasonal_fruit = String::from("blueberries");
+}
 
 #[cfg(test)]
 mod tests {
